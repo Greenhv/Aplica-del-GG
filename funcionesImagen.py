21dcf53270ee90763c8d577955f8636f8c29c6ca
Herbert V <hvidelac@gmail.com>
2015-04-21 16:16:25 -0500

    No la valgo :(

--- funcionesImagen.py
+++ funcionesImagen.py
@@ -1,6 +1,7 @@
 from cv2  import *
 from numpy import *
 import glob
+import os
 
 
 def lecturaEscrituraImagen(name):   
@@ -9,42 +10,53 @@ def lecturaEscrituraImagen(name):
     return img
 
 
-def testLecturaEscrituraImagen():
-    name = 'darthvader'        
+def testLecturaEscrituraImagen(name):
+    #name = 'darthvader'        
     img =  lecturaEscrituraImagen(name)
     if(img==None):
         print('No hay imagen')
         return
-    imwrite('salida/'+name,img)        
+
+    if not os.path.exists('salida'):
+        os.makedirs('salida')
+    imwrite('salida/'+name.split('.')[0]+'.png',img)        
     #imshow('DarthVader',img)    
     #waitKey(0)    
     
 
 def modificacionPixeles(img):
-    nuevo = img
-
-    x,y = img.shape[:2]
-    
-    for i in x:
-        for j in y:
-            nuevo[i][j] = 255 - img[i][j]
-    #imwrite(nuevo,'salida/ej2'+name+'Inv.png')
+    nuevo = 255 - img
     return nuevo
+
+def testModificacionPixeles(nombre):
+    imgIn = imread(nombre,1)
+    imgOut = modificacionPixeles(imgIn)
+    if not os.path.exists('salida'):
+        os.makedirs('salida')
+    imwrite('salida/ej2' + nombre.split('.')[0] + 'Inv.png',imgOut)
 
 
 def espejarImagen(img):
-    nuevo = img
+    nuevo = img.copy()
     
     x,y = img.shape[:2]
 
-    for i in x:
-        for j in y:
-            nuevo[i][j] = img[i][y-1-j]        
+    for i in range(x):
+        z = y/2
+        for j in range(z):			
+            pix = nuevo[i][j].copy()
+            nuevo[i][j] = nuevo[i][y-1-j].copy()
+            nuevo[i][y-1-j] = pix
 
     #imwrite(nuevo,'salida/ej3'+name+'Espejo.png' )
     return nuevo
 
-
+def testEspejoImagen(nombre):
+    imgIn = imread(nombre,1)
+    imgOut = espejarImagen(imgIn)
+    if not os.path.exists('salida'):
+        os.makedirs('salida')
+    imwrite('salida/ej3'+nombre.split('.')[0]+'Espejo.png',imgOut)
 
 def calcularHistograma(img, tipo):
 
@@ -54,10 +66,10 @@ def calcularHistograma(img, tipo):
             L.append(0)    
 
         x,y = img.shape[:2]
-        for i in x:
-            for j in y:
-                tonogris = (int)(img[i][j][0]*0.11 + img[i][j][1]*0.59 + img[i][j][2]*0.3)
-                L[ tonogris ]+=1                            
+        for i in range(x):
+            for j in range(y):
+                tonogris = int((img[i][j][0]*0.11 + img[i][j][1]*0.59 + img[i][j][2]*0.3))
+                L[ tonogris ]+=1        
         return L
           
     elif(tipo=='RGB'):
@@ -66,27 +78,36 @@ def calcularHistograma(img, tipo):
             L.append(0)
         
         x,y = img.shape[:2]
-        for i in x:
-            for j in y:
+        for i in range(x):
+            for j in range(y):
                 for k in range(3):
-                    L[ k*256 + img[i][j][k] ]+=1                            
+                    L[ (2-k)*256 + img[i][j][k] ]+=1                            
         return L
         # Detalle creo que la salida es BGR xD
 
-#Todo compila pero falta funciones test 
+def testHistogramaImagen(name,tipo):
 
+	imgIn = imread(name,1)
+	L = calcularHistograma(imgIn,tipo)
+
+	print L
 
 def caracterizarImagenes( carpeta, cad ):
 
     M = []    
     
-    for name in glob.glob(carpeta+'/*.jpg'):
-        img = lecturaEscrituraImagen(name)   
-        if(  img==None):
-            break
-        
-        M.append( calcularHistograma(img,cad) )                              
-    savetxt('salida/'+carpeta+'.csv', M )
+    for name in glob.glob(carpeta+'/*.jpg'):     
+        img = lecturaEscrituraImagen(name)    
+        M.append( calcularHistograma(img,cad) )
+        #M.append(carpeta)
+    
+        #print name
+
+    if not os.path.exists('salida'):
+        os.makedirs('salida')
+    savetxt('salida/'+carpeta+'.csv', M,fmt='%d',delimiter=',' )
 
 
+def testCaracterizarImagenes(name,tipo):
+	caracterizarImagenes(name,tipo)
 
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from funcionesImagen import *
 
+#name = raw_input('Ingrese el nombre de la imagen: ')
 
+#tipo = raw_input('Ingrese GRAYSCALE o RGB:')
 
-testLecturaEscrituraImagen()    
+#testLecturaEscrituraImagen(name)
 
+#testModificacionPixeles(name)
 
+#testEspejoImagen(name)
 
+#testHistogramaImagen(name,tipo)
 
+while True:
+
+    nombreCarpeta = raw_input('Ingrese el nombre de la carpeta: ')
+
+    tipoImagen = raw_input('Ingrese GRAYSCALE o RGB:')
+    
+    testCaracterizarImagenes(nombreCarpeta,tipoImagen)
+    
+    door = raw_input('Existen mas carpetas? (Y/N): ')
+    
+    if door=='N':
+        break
