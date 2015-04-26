8c7893e60c39f1ee5c9c589c44a4ac4edbc64628
Herbert V <hvidelac@gmail.com>
2015-04-25 20:14:35 -0500

    Ya hace algo :D , ahora falta que todo este mal xd . Go lunes luego del almuerzo

--- Lab4/funciones.py
+++ Lab4/funciones.py
@@ -1,11 +1,8 @@
 import numpy as np 
 
-def distanciaE(x,y): 
-    #Considerando como puntos iniciales x1 = 0 e y1 = 0 
-    x1 = 0 
-    y1 = 0 
-    difX = (x - x1)*(x - x1)
-    difY = (y - y1)*(y - y1) 
+def distanciaE(pos1,posPiv):      
+    difX = (posPiv[0] - pos1[0])*(posPiv[0] - pos1[0])
+    difY = (posPiv[1] - pos1[1])*(posPiv[1] - pos1[1])
     rep = np.sqrt(difX+difY) 
     return rep
 
--- Lab4/main.py
+++ Lab4/main.py
@@ -8,32 +8,42 @@ def main():
         print("Ingrese la cantidad de argumentos (3)")
         return 0
     
-    n=sys.argv[1]
-    k=sys.argv[2]
+    n=int(sys.argv[1])
+    k=int(sys.argv[2])
     filename=sys.argv[3]
     print "correcto"
     f = open(filename,'r')
     L = []
+    pivotes = []
+    cluster = [[],[],[]]
+    datosClus = []
     #Carga la matriz en una lista , cada elemento es una fila del archivo
     # de csv    
     for line in f:
-        R = line[0:len(line)-1].split(',')
+        R = map(int,line[:-2].split(','))
+        if len(R) > n:
+            print "Se detecto un numero incorrecto de atributos"
+            exit()
         L.append(R)
+    #Calcula la posicion de los 3 pivotes dentro del arreglo L.
+    for i in range(k):        
+        pivotes.append(rm.randint(0,len(L)))    
     
-    pivot1 = rm.randint(0,len(L))
-    pivot2 = rm.randint(0,len(L))    
-    pivot3 = rm.randint(0,len(L))
-    
-       
-        
-         
-    
-              
-        
+    for i in range(len(L)):
+        u = 0
+        minima = fp.distanciaE(L[i],L[pivotes[0]])
+        for j in range(1,k):
+            dist = fp.distanciaE(L[i],L[pivotes[j]]) 
+            if dist < minima:
+                minima = dist
+                u = j
+        cluster[u].append(L[i])
+        #L[i] = [0]*n 
+   
 
 
 
 
     
-if __name__=="__main__" :
+if __name__=="__main__":
     main()
